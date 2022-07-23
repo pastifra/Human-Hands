01c2a15ea6e60eb25878d0aefdd3d3016ebc9bd1
Anna Furciniti <103197530+annafurciniti@users.noreply.github.com>
2022-07-23 23:20:31 +0200

    Update segmentation_.cpp
    color hands start

--- Human-Hands-CPP/source/segmentation_.cpp
+++ Human-Hands-CPP/source/segmentation_.cpp
@@ -33,7 +33,7 @@ int main( int argc, char** argv ){
             Mat mask1 = Mat::zeros(img.rows,img.cols, CV_8UC1);//all black
             Rect r = Rect(numbers.at(i),numbers.at(i+1), numbers.at(i+2), numbers.at(i+3));
 
-            //improveBySkin(img);
+            improveBySkin(r, img);
             segmentation_rect(img, r, mask1);
             improveMask(r, mask1);
             segmentation_mask(img, mask1);
@@ -45,15 +45,31 @@ int main( int argc, char** argv ){
             
             mask1 = (mask1==1)+(mask1==3);
             mask = mask + mask1;
-            img.copyTo(dst, mask);
+            //img.copyTo(dst, mask);
+            /*
+            dilate(mask, mask, cv::Mat());
+            erode(mask, mask, cv::Mat());*/
+            
+            //color randomically the masks not complete
+            vector<Mat> masks;
+            masks.push_back(mask);
+            
+            for(size_t l = 0; l < masks.size(); l++){
+                Scalar random = Scalar(rand()%255,rand()%255,rand()%255);
+                img.setTo(random, mask);
+                img.setTo(random, mask);
+                imshow("dst", img);
+                waitKey(1);
+        
+            }
         }
     
-        Mat dst_got;
+        /*Mat dst_got;
         img_got.copyTo(dst_got, mask_got);
 
 
         imshow("mask", mask);
-        waitKey(0);
+        waitKey(0);*/
         cout<<"Pixel accuracy:"<< pixelAccuracy(mask, mask_got)<<endl;
     }
     return 0;
