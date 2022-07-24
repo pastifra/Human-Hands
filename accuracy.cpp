f49b114bf4e412ade6d8e8e739a5a2fd37e088cd
marinaceccon1 <107349467+marinaceccon1@users.noreply.github.com>
2022-07-24 18:12:50 +0200

    Add files via upload

---
+++ accuracy.cpp
@@ -0,0 +1,28 @@
+#include "segmentation.h"
+
+using namespace std;
+using namespace cv;
+
+double pixelAccuracy(Mat& mask, Mat& mask_gt){
+    double TP, TN, FP, FN;
+    for(int i = 0; i < mask.rows; i++){//got
+        for(int j = 0; j < mask.cols; j++){
+           if(mask.at<Vec3b>(i,j)[0] == 255 && mask.at<Vec3b>(i,j)[1] == 255 && mask.at<Vec3b>(i,j)[3] == 255){
+                if(mask_gt.at<Vec3b>(i,j)[0] == 255 && mask_gt.at<Vec3b>(i,j)[1] == 255 && mask_gt.at<Vec3b>(i,j)[3] == 255){//correctly classified as hands
+                    TP++;
+                }
+                else{//incorrectly classified as hands
+                    FP++;
+                }
+            }else if(mask_gt.at<Vec3b>(i,j)[0] == 255 && mask_gt.at<Vec3b>(i,j)[1] == 255 && mask_gt.at<Vec3b>(i,j)[3] == 255){//incorrectly classified as not hands
+                    FN++;  
+                }
+                else{//correctly classified as not hands
+                    TN++;
+                }
+         }
+    }
+        
+    //calculate accuracy
+    return(TP + TN)/(TP + TN + FP +FN);
+    }
