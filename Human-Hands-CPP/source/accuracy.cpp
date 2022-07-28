b4f599f2bd9896f31688c98a91ea56aef313a2f3
Anna Furciniti <103197530+annafurciniti@users.noreply.github.com>
2022-07-28 21:58:14 +0200

    Update accuracy.cpp
    bug correction

--- Human-Hands-CPP/source/accuracy.cpp
+++ Human-Hands-CPP/source/accuracy.cpp
@@ -5,7 +5,10 @@ using namespace std;
 using namespace cv;
 
 double pixelAccuracy(Mat& mask, Mat& mask_gt){
-    double TP, TN, FP, FN;
+    double TP = 0;
+    double TN = 0;
+    double FP = 0;
+    double FN = 0;
     for(int i = 0; i < mask.rows; i++){
         for(int j = 0; j < mask.cols; j++){
            if(mask.at<Vec3b>(j,i)[0] == 255 && mask.at<Vec3b>(j,i)[1] == 255 && mask.at<Vec3b>(j,i)[3] == 255){
